18ec2da40b109290d91006a4c92d34c437c78496
lismgmk <lismgmk2@gmail.com>
2021-02-19 22:42:05 +0300

    Lesson 11

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app-wrapper">
+        <header>
+
+            <img src='https://banner2.cleanpng.com/20191009/qxh/transparent-social-icon-social-icon-vk-social-logotype-icon-5da68002926890.9161733415711928345997.jpg'/>
+            <h1>Hello</h1>
+        </header>
+        <nav>
+            <li>Profile</li>
+            <li>Message</li>
+            <li>News</li>
+            <li>Music</li>
+            <li>Setting</li>
+        </nav>
+        <div className='main'>Main Content</div>
+    </div>
+  );
+}
+
+;
+
+export default App;
+
